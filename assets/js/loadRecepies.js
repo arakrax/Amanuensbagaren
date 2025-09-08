5e35a470989574fdc8414a39e8ef95e59bc9f4f6
arakrax <andp.larsson@gmail.com>
2025-09-08 14:55:14 +0200

    added recipes

--- assets/js/loadRecepies.js
+++ assets/js/loadRecepies.js
@@ -9,15 +9,17 @@ function populatePage(recipes) {
         let recipe = await getRecipe(recipeID)
         let h3 = document.createElement("h3");
         h3.textContent = recipe.name;
-        let p = document.createElement("p");
-        p.textContent = recipe.portions;
+        let p1 = document.createElement("p");
+        p1.textContent = recipe.portions;
+        let p2 = document.createElement("p");
+        p2.textContent = recipe.description;
         let img = document.createElement("img");
         img.src = recipe.img_path;
         img.alt = recipe.img_alt;
         let a = document.createElement("a");
         a.href = `${recipePath}?id=${recipeID}`;
         
-        a.append(img, h3, p);
+        a.append(img, h3, p1, p2);
         recipeWidget.append(a)
     });
 }
